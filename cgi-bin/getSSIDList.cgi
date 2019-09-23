ddf3356a6e5dcf002ce3b21fa52c2a6862eb7dc0
shachindra <shachindra.spidey@gmail.com>
2019-09-23 15:26:35 +0000

    Initial Commit

---
+++ cgi-bin/getSSIDList.cgi
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+echo -e "Content-type: text/plain\n"
+printf '\r\n'
+
+printf "SSID 1,SSID 2,SSID3"
--- saveAuth.js
+++ saveAuth.js
@@ -1,10 +1,28 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+		var url = "/cgi-bin/getSSIDList.cgi";
+		var xmlHttp = new XMLHttpRequest();
+		xmlHttp.open("GET", url, false);	// false: wait respond
+		xmlHttp.send(null);
+		console.log(xmlHttp.responseText);
+		var myobject = {
+			ValueA: 'Text A',
+			ValueB: 'Text B',
+			ValueC: 'Text C'
+		};
+		var select = document.getElementById("selectWifi");
+		for (index in myobject) {
+			select.options[select.options.length] = new Option(myobject[index], index);
+		}
+	});
+
 function httpGET(url) {
+		console.log("API Query");
 		var xmlHttp = new XMLHttpRequest();
 		xmlHttp.open("GET", url, true);	// false: wait respond
 		xmlHttp.send(null);
 		return url+" "+xmlHttp.responseText;
 }
 function saveSettings() {
-		var url = "./cgi-bin/saveAuthConfig.cgi?" + document.getElementById('username').value + "\\" + document.getElementById('password').value;
+		var url = "/cgi-bin/saveAuthConfig.cgi?" + document.getElementById('username').value + ";" + document.getElementById('password').value;
 		document.getElementById('info-div').innerHTML = httpGET(url);
 }
